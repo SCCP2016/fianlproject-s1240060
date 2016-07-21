67c73cd8a2eff76568a9bd32cb7a68b1608b987a
s1240060_yamada <s1240060@u-aizu.ac.jp>
2016-07-21 18:28:25 +0900

    first commit

---
+++ src/exdirmng.rb
@@ -0,0 +1,14 @@
+#!/usr/local/gnu/bin/ruby
+
+class CommandParser
+    def initialize(argv)
+        argv= @argv
+    end
+    def parse()
+        #
+    end
+end
+
+class Command
+    def initialize()
+        type= 
